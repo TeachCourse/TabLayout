4c207bb7c65127e24e57970c6eef046910714788
Administrator <123456>
2018-05-08 14:01:22 +0800

    optimize and add other attributes

--- src/main/java/cn/teahcourse/tablayout/TabLayout.java
+++ src/main/java/cn/teahcourse/tablayout/TabLayout.java
@@ -45,8 +45,8 @@ import android.widget.TextView;
 import java.util.Locale;
 
 
-public class TabNavigation extends HorizontalScrollView {
-    private static final String TAG = "PagerSlidingTabStrip";
+public class TabLayout extends HorizontalScrollView {
+    private static final String TAG = "TabLayout";
 
     public interface IconTabProvider {
         int getPageIconResId(int position);
@@ -84,15 +84,16 @@ public class TabNavigation extends HorizontalScrollView {
     private boolean obtainTextWidth = false;
 
     private int scrollOffset = 52;
-    private int indicatorHeight = 8;
+    private int indicatorHeight = 4;
     private int underlineHeight = 2;
+    private int indicatorWidth = -1;
     private int dividerPadding = 12;
     private int tabPadding = 24;
     private int dividerWidth = 1;
 
-    private int tabTextSize = 12;
-    private int tabTextColor = 0xFF666666;
-    private int selectedTabTextColor = 0xFF666666;
+    private int textSize = 12;
+    private int textColor = 0xFF666666;
+    private int textColorSelected = 0xFF666666;
     private int tabBackgroundResId = R.drawable.background_tab;
     private int lastScrollX = 0;
 
@@ -103,15 +104,15 @@ public class TabNavigation extends HorizontalScrollView {
 
     private Shader mShader;
 
-    public TabNavigation(Context context) {
+    public TabLayout(Context context) {
         this(context, null);
     }
 
-    public TabNavigation(Context context, AttributeSet attrs) {
+    public TabLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public TabNavigation(Context context, AttributeSet attrs, int defStyle) {
+    public TabLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
         setFillViewport(true);
@@ -160,57 +161,65 @@ public class TabNavigation extends HorizontalScrollView {
                 TypedValue.COMPLEX_UNIT_DIP, tabPadding, dm);
         dividerWidth = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, dividerWidth, dm);
-        tabTextSize = (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_SP, tabTextSize, dm);
+        textSize = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, textSize, dm);
 
         // get system attrs (android:textSize and android:textColor)
 
         TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
         //获取配置文件属性值
-        tabTextSize = a.getDimensionPixelSize(0, tabTextSize);
-        tabTextColor = a.getColor(1, tabTextColor);
+        textSize = a.getDimensionPixelSize(0, textSize);
+        textColor = a.getColor(1, textColor);
 
         a.recycle();
 
         // get custom attrs
 
         a = context.obtainStyledAttributes(attrs,
-                R.styleable.TabNavigation);
+                R.styleable.TabLayout);
         //获取配置文件属性值
         indicatorColor = a.getColor(
-                R.styleable.TabNavigation_tab_indicator_color,
+                R.styleable.TabLayout_tab_indicator_color,
                 indicatorColor);
         underlineColor = a.getColor(
-                R.styleable.TabNavigation_tab_underline_color,
+                R.styleable.TabLayout_tab_underline_color,
                 underlineColor);
         dividerColor = a
-                .getColor(R.styleable.TabNavigation_tab_divider_color,
+                .getColor(R.styleable.TabLayout_tab_divider_color,
                         dividerColor);
         indicatorHeight = a.getDimensionPixelSize(
-                R.styleable.TabNavigation_tab_indicator_height,
+                R.styleable.TabLayout_tab_indicator_height,
                 indicatorHeight);
+        indicatorWidth = a.getDimensionPixelSize(
+                R.styleable.TabLayout_tab_indicator_width,
+                indicatorWidth);
         underlineHeight = a.getDimensionPixelSize(
-                R.styleable.TabNavigation_tab_underline_height,
+                R.styleable.TabLayout_tab_underline_height,
                 underlineHeight);
         dividerPadding = a.getDimensionPixelSize(
-                R.styleable.TabNavigation_tab_divider_padding,
+                R.styleable.TabLayout_tab_divider_padding,
                 dividerPadding);
         tabPadding = a.getDimensionPixelSize(
-                R.styleable.TabNavigation_tab_padding_left_to_right,
+                R.styleable.TabLayout_tab_padding_left_to_right,
                 tabPadding);
         tabBackgroundResId = a.getResourceId(
-                R.styleable.TabNavigation_tab_background,
+                R.styleable.TabLayout_tab_background,
                 tabBackgroundResId);
         shouldExpand = a
-                .getBoolean(R.styleable.TabNavigation_tab_should_expand,
+                .getBoolean(R.styleable.TabLayout_tab_should_expand,
                         shouldExpand);
         scrollOffset = a
                 .getDimensionPixelSize(
-                        R.styleable.TabNavigation_tab_scroll_offset,
+                        R.styleable.TabLayout_tab_scroll_offset,
                         scrollOffset);
         textAllCaps = a.getBoolean(
-                R.styleable.TabNavigation_tab_text_all_caps, textAllCaps);
-
+                R.styleable.TabLayout_tab_text_all_caps, textAllCaps);
+        textColorSelected = a.getColor(R.styleable.TabLayout_tab_text_color_selected,
+                textColorSelected);
+        textColor=a.getColor(R.styleable.TabLayout_tab_text_color,
+                textColor);
+        textSize=a.getDimensionPixelSize(R.styleable.TabLayout_tab_text_color,
+                textSize);
 
         a.recycle();
     }
@@ -305,9 +314,9 @@ public class TabNavigation extends HorizontalScrollView {
             if (v instanceof TextView) {
 
                 TextView tab = (TextView) v;
-                tab.setTextSize(TypedValue.COMPLEX_UNIT_PX, tabTextSize);
+                tab.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
                 tab.setTypeface(tabTypeface, tabTypefaceStyle);
-                tab.setTextColor(tabTextColor);
+                tab.setTextColor(textColor);
 
                 // setAllCaps() is only available from API 14, so the upper case
                 // is made manually if we are on a
@@ -321,7 +330,7 @@ public class TabNavigation extends HorizontalScrollView {
                     }
                 }
                 if (i == selectedPosition) {
-                    tab.setTextColor(selectedTabTextColor);
+                    tab.setTextColor(textColorSelected);
                 }
             }
         }
@@ -355,17 +364,56 @@ public class TabNavigation extends HorizontalScrollView {
         if (isInEditMode() || tabCount == 0) {
             return;
         }
-
         final int height = getHeight();
 
+        drawUnderline(canvas, height);
+
+        drawIndicator(canvas, height);
+
+        drawDivider(canvas, height);
+    }
+
+    /**
+     * 绘制指示器之间的分隔线，自定义颜色、内边距
+     *
+     * @param canvas
+     * @param height
+     */
+    private void drawDivider(Canvas canvas, int height) {
+        // draw divider
+
+        dividerPaint.setColor(dividerColor);
+        for (int i = 0; i < tabCount - 1; i++) {
+            View tab = tabsContainer.getChildAt(i);
+            canvas.drawLine(tab.getRight(), dividerPadding, tab.getRight(),
+                    height - dividerPadding, dividerPaint);
+        }
+    }
+
+    /**
+     * 绘制导航底部下划线，自定义颜色、高度
+     *
+     * @param canvas
+     * @param height
+     */
+
+    private void drawUnderline(Canvas canvas, int height) {
         // draw underline
         rectPaint.setColor(underlineColor);
         canvas.drawRect(0, height - underlineHeight, tabsContainer.getWidth(),
                 height, rectPaint);
+    }
 
+    /**
+     * 绘制选中文字底部下划线，自定义颜色
+     *
+     * @param canvas
+     * @param height
+     */
+    private void drawIndicator(Canvas canvas, int height) {
         // draw indicator line
         rectPaint.setColor(indicatorColor);
-//        dividerPaint.setShader(mShader);
+        dividerPaint.setShader(mShader);
 
         // default: line below current tab
         View currentTab = tabsContainer.getChildAt(currentPosition);
@@ -375,32 +423,25 @@ public class TabNavigation extends HorizontalScrollView {
         // if there is an offset, start interpolating left and right coordinates
         // between current and next tab
 
+        int width = currentTab.getWidth();
         if (currentPositionOffset > 0f && currentPosition < tabCount - 1) {
 
             View nextTab = tabsContainer.getChildAt(currentPosition + 1);
             final float nextTabLeft = nextTab.getLeft();
             final float nextTabRight = nextTab.getRight();
 
-            lineLeft = (currentPositionOffset * nextTabLeft + (1f - currentPositionOffset)
-                    * lineLeft);
-            lineRight = (currentPositionOffset * nextTabRight + (1f - currentPositionOffset)
-                    * lineRight);
+            lineLeft = (currentPositionOffset * nextTabLeft + (1f - currentPositionOffset) * lineLeft);
+            lineRight = (currentPositionOffset * nextTabRight + (1f - currentPositionOffset) * lineRight);
+
+            width = nextTab.getWidth();
         }
-
-        //绘制选中tab指示器矩形
-        canvas.drawRect(lineLeft, height - indicatorHeight, lineRight, height,
-                rectPaint);
-
-
-        // draw divider
-
-        dividerPaint.setColor(dividerColor);
-        for (int i = 0; i < tabCount - 1; i++) {
-            View tab = tabsContainer.getChildAt(i);
-            canvas.drawLine(tab.getRight(), dividerPadding, tab.getRight(),
-                    height - dividerPadding, dividerPaint);
+        //绘制选中tab指示器底部下划线，默认为当前Tab宽度的一半
+        if (indicatorWidth == -1)
+            canvas.drawRect(lineLeft + width / 4, height - indicatorHeight, lineRight - width / 4, height, rectPaint);
+        else if (indicatorWidth < width && indicatorWidth > 0) {
+            int offset = width - indicatorWidth;
+            canvas.drawRect(lineLeft + offset / 2, height - indicatorHeight, lineRight - offset / 2, height, rectPaint);
         }
-
     }
 
     private class PageListener implements OnPageChangeListener {
@@ -417,13 +458,13 @@ public class TabNavigation extends HorizontalScrollView {
             invalidate();
 
             if (delegatePageListener != null) {
-                delegatePageListener.onPageScrolled(position, positionOffset,
-                        positionOffsetPixels);
+                delegatePageListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
             }
 
             Log.e(TAG, positionOffset + "");
-            int colors[] = new int[]{Color.GREEN,
-                    Color.TRANSPARENT};
+            int colors[] = new int[]{Color.GREEN, Color.TRANSPARENT};
+
+            //设置切换过程中的渐变色
             mShader = new LinearGradient(0, 0, positionOffset, 90, colors, null, Shader.TileMode.REPEAT);
         }
 
@@ -504,7 +545,7 @@ public class TabNavigation extends HorizontalScrollView {
         };
     }
 
-    public static class Builder{
+    public static class Builder {
         private Context context;
         private int indicatorColor = 0xFF666666;
         private int underlineColor = 0x1A000000;
@@ -609,8 +650,9 @@ public class TabNavigation extends HorizontalScrollView {
             this.tabBackgroundResId = tabBackgroundResId;
             return this;
         }
-        public TabNavigation create(){
-            TabNavigation navigation=new TabNavigation(context);
+
+        public TabLayout create() {
+            TabLayout navigation = new TabLayout(context);
             navigation.indicatorColor = indicatorColor;
             navigation.underlineColor = underlineColor;
             navigation.dividerColor = dividerColor;
@@ -626,9 +668,9 @@ public class TabNavigation extends HorizontalScrollView {
             navigation.tabPadding = tabPadding;
             navigation.dividerWidth = dividerWidth;
 
-            navigation.tabTextSize = tabTextSize;
-            navigation.tabTextColor = tabTextColor;
-            navigation.selectedTabTextColor = selectedTabTextColor;
+            navigation.textSize = tabTextSize;
+            navigation.textColor = tabTextColor;
+            navigation.textColorSelected = selectedTabTextColor;
             navigation.tabBackgroundResId = tabBackgroundResId;
             return navigation;
         }
